a09d29b3fe9c28a44b8a16e0a7ab573c8b0b0363
李喆宸 <leeggroy@gmail.com>
2025-11-16 22:02:42 -0500

    fix: Use npx @argos-ci/cli for screenshot upload1

--- argos.config.js
+++ argos.config.js
@@ -1,4 +1,4 @@
 export default {
-  // Screenshots are uploaded from ./screenshots directory
-  // This is configured in the GitHub Action
+  // Root directory for screenshots
+  root: './screenshots',
 };
